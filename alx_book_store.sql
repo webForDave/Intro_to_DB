80fc5c11224fa6ca09b606ef6026bfb9d72c2a9d
David Akinola <dakinola54@gmail.com>
2025-06-29 01:20:48 +0100

    Created table for order details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,4 +30,11 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
